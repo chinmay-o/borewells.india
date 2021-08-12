e53ad3d0dfc76f379f94c85d39f68d53bebcefa3
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-12 22:48:09 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,10 +4,12 @@
 $("#contact_form").submit((e) => {
   e.preventDefault()
   $.ajax({
-    headers: {'Access-Control-Allow-Origin': 'https://chinmay-o.github.io/borewells.india/'},
+
     url: "https://script.google.com/macros/s/AKfycby2QsXGw-N_oXFgZ7_ARvhsaDpbu6S4YMaUrQ-ckfXj797pxBssm8k3M21U4ru6w3ZpZg/exec",
     data: $("#contact_form").serialize(),
     method: "post",
+    headers: {'Access-Control-Allow-Origin': 'https://chinmay-o.github.io/borewells.india/'},
+    
     success: function(response) {
       alert("Form submitted successfully")
       window.location.reload()
