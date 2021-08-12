d9918a971636feb2161fe5e8c3d57c1f1a5cc1f2
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-12 22:40:13 +0530

    beta V1.0.2

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,12 @@
 
 
-$("#submit-form").submit((e) => {
+
+$("#contact_form").submit((e) => {
   e.preventDefault()
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbyTot7erA95oAWu7CCB2nuDxLpo8LQdzmLLJ8GSLeL1J-kFI-FqVXEb9Iz2AIPQwAxmCQ/exec",
-    data: $("#submit-form").serialize(),
+    headers: {'Access-Control-Allow-Origin': 'https://chinmay-o.github.io/borewells.india/'},
+    url: "https://script.google.com/macros/s/AKfycby2QsXGw-N_oXFgZ7_ARvhsaDpbu6S4YMaUrQ-ckfXj797pxBssm8k3M21U4ru6w3ZpZg/exec",
+    data: $("#contact_form").serialize(),
     method: "post",
     success: function(response) {
       alert("Form submitted successfully")
@@ -13,7 +15,6 @@ $("#submit-form").submit((e) => {
     },
     error: function(err) {
       alert("Something Error")
-
-    }
+    },
   })
 });
