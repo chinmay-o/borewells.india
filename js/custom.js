0d16664a66696b3d6956f3f92fb6bd111b0dc47a
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-12 20:40:40 +0530

    beta V1.0.1
    minor

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,19 @@
 
 
-$("#contact_form").submit((e)=>{
-		e.preventDefault()
-		$.ajax({
-				url:"https://script.google.com/macros/s/AKfycbygBzeC-fA2akL22No-F5luKrd0jpKbZTmb26LGtVQneNSP-XpiZqJ0uNvVOil8slmnFg/exec",
-				data:$("#contact_form").serialize(),
-				method:"POST",
-				success:function (response){
-						alert("Successfull");
-				},
-				error:function (err){
-						alert("Something Error");
-				}
-		})
-})
+$("#submit-form").submit((e) => {
+  e.preventDefault()
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbyTot7erA95oAWu7CCB2nuDxLpo8LQdzmLLJ8GSLeL1J-kFI-FqVXEb9Iz2AIPQwAxmCQ/exec",
+    data: $("#submit-form").serialize(),
+    method: "post",
+    success: function(response) {
+      alert("Form submitted successfully")
+      window.location.reload()
+      //window.location.href="https://google.com"
+    },
+    error: function(err) {
+      alert("Something Error")
+
+    }
+  })
+});
