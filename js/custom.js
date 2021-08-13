65b3a838545b7ada3dbd0bb8e8cfd6377a70ba72
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-13 13:43:23 +0530

    form

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,9 @@
 $("#contact_form").submit((e) => {
   e.preventDefault()
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbwU2dHQ-b0LFjy2VZYwTxiMjID_X990Ma3_OYGMCv2N_bBWVLYRHNDNumuH3j85Lv1g/exec",
-    data: $("#contact_form").serialize(),
+    url: "https://formsubmit.co/borewellsind@gmail.com",
     method: "POST",
-    
+
     success: function(response) {
       alert("Form submitted successfully")
       // window.location.reload()
