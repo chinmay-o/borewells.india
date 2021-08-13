8e40bee5bceae9de5272ddf3fbcb11d09ee13c45
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-13 12:42:03 +0530

    forms

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $("#contact_form").submit((e) => {
     url: "https://script.google.com/macros/s/AKfycbwU2dHQ-b0LFjy2VZYwTxiMjID_X990Ma3_OYGMCv2N_bBWVLYRHNDNumuH3j85Lv1g/exec",
     data: $("#contact_form").serialize(),
     method: "POST",
-
+    
     success: function(response) {
       alert("Form submitted successfully")
       // window.location.reload()
