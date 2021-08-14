94b83d91254cfaa6c82762e455a51c786137a7c7
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-14 23:39:03 +0530

    beta V2.0.3
    minor changes

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,24 @@ $("#submit-form").submit((e) => {
     type: "POST",
     success: function(response) {
       alert("Form submitted successfully")
+      // window.location.reload()
+      //window.location.href="https://google.com"
+    },
+    error: function(err) {
+      alert("Something Error")
+
+    }
+  })
+})
+
+$("#submit-form-popup").submit((e) => {
+  e.preventDefault()
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbx80Pv_KT6vwzsQB2klyDHSRZQaEupOkBRfB3XTBGzkBt6TBffh2guDVjh-YvGuPIwp/exec",
+    data: $("#submit-form-popup").serialize(),
+    type: "POST",
+    success: function(response) {
+      alert("Form submitted successfully")
       window.location.reload()
       //window.location.href="https://google.com"
     },
