720f7dddef4ba708338da475d8c478063a4d8e63
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-08-15 14:17:59 +0530

    V3.0
    Hosting Version 1

--- js/custom.js
+++ js/custom.js
@@ -7,13 +7,13 @@ $("#submit-form").submit((e) => {
     data: $("#submit-form").serialize(),
     type: "POST",
     success: function(response) {
-      alert("Form submitted successfully")
-      // window.location.reload()
-      //window.location.href="https://google.com"
+      $(".success-message").css("display", "block");
+      $('#submit-form')[0].reset();
+      fadeOut(".success-message");
     },
     error: function(err) {
-      alert("Something Error")
-
+      $(".error-message").css("display", "block");
+      fadeOut(".error-message");
     }
   })
 })
@@ -25,13 +25,51 @@ $("#submit-form-popup").submit((e) => {
     data: $("#submit-form-popup").serialize(),
     type: "POST",
     success: function(response) {
-      alert("Form submitted successfully")
-      window.location.reload()
-      //window.location.href="https://google.com"
+      $(".success-message-modal").css("display", "block");
+      $('#submit-form-popup')[0].reset();
+      fadeOut(".success-message-modal");
     },
     error: function(err) {
-      alert("Something Error")
-
+      $(".error-message-modal").css("display", "block");
+      fadeOut(".error-message-modal");
     }
   })
 })
+
+function fadeOut(className) {
+  setTimeout(function(){
+    $(className).remove();
+  }, 6000);
+}
+
+$("#footer-about").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".section-about").offset().top - 60
+      }, 1000);
+});
+
+$("#footer-services").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".row-project-box").offset().top - 200
+      }, 1000);
+});
+
+$("#footer-testimonials").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".section-clients").offset().top - 60
+      }, 1000);
+});
+
+$("#footer-contact").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".row-contact").offset().top - 160
+      }, 1000);
+});
+
+$('.brand').click(function() {
+    location.reload();
+});
